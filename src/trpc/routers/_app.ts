2590bcb5a2fddd453ae1e7b76190cdc49857da41
clevermiraz <miraz.onesoft@gmail.com>
2025-11-30 15:43:58 +0600

    Better Auth Authentication Setup ☺️

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import prisma from "@/lib/db";
-import { baseProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(() => {
+  getUsers: protectedProcedure.query(({ ctx }) => {
+    console.log({ userId: ctx.auth.user.id });
+
     return prisma.user.findMany();
   }),
 });
