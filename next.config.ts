02f11a451f0a41319cff1db00be35970eadbb25f
clevermiraz <miraz.onesoft@gmail.com>
2025-12-16 14:53:47 +0600

    fix prisma invoking error

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+
+  outputFileTracingIncludes: {
+    "/api/*": ["./node_modules/.prisma/client/*.wasm"],
+  },
 };
 
 export default withSentryConfig(nextConfig, {
