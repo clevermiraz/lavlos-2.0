f4bae881ea0bb939b75b896a6d806c7a122dfe65
clevermiraz <miraz.onesoft@gmail.com>
2025-12-16 15:03:22 +0600

    revert back prisma

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-
-  outputFileTracingIncludes: {
-    "/api/*": ["./node_modules/.prisma/client/*.wasm"],
-  },
 };
 
 export default withSentryConfig(nextConfig, {
